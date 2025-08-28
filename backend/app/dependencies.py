0de8381e9cb0d2e58774e636cad9af6879190721
Luke <lukegtseng@gmail.com>
2025-08-28 05:08:44 -0700

    added game_service_factory for getting game objects from game type
    marking values in classes as private
    
    Adding get blob function and game id to game state

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,44 +1,48 @@
 from functools import lru_cache
 
-from app.services.cosmos_service import CosmosService
 from app.services.blob_service import BlobService
-from app.services.redis_service import RedisService
-from app.services.user_service import UserService
-from app.services.room_service import RoomService
 from app.services.connection_service import ConnectionService
+from app.services.cosmos_service import CosmosService
+from app.services.game_service_factory import GameServiceFactory
+from app.services.redis_service import RedisService
+from app.services.room_service import RoomService
+from app.services.user_service import UserService
 
-# TEMP IMPORT FOR TESTING
-from app.services.games.chess_game import ChessLogic
 
 @lru_cache
 def get_cosmos_service() -> CosmosService:
     return CosmosService()
 
+
 @lru_cache
 def get_blob_service() -> BlobService:
     return BlobService()
+
 
 @lru_cache
 def get_redis_service() -> RedisService:
     return RedisService()
 
+
+@lru_cache
+def get_game_service_factory() -> GameServiceFactory:
+    return GameServiceFactory()
+
+
 @lru_cache
 def get_connection_service() -> ConnectionService:
     return ConnectionService(redis_service=get_redis_service())
 
+
 @lru_cache
 def get_user_service() -> UserService:
     return UserService(cosmos_service=get_cosmos_service())
+
 
 @lru_cache
 def get_room_service() -> RoomService:
     return RoomService(
         cosmos_service=get_cosmos_service(),
         redis_service=get_redis_service(),
-        connection_service=get_connection_service()
+        connection_service=get_connection_service(),
     )
-
-# TEMP, REPLACE WITH ACTUAL GAME SERVICE
-@lru_cache
-def get_game_service() -> ChessLogic:
-    return ChessLogic()
