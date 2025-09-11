6b130489e2922f30ee3acf7c82b28b968d4eb6e6
Luke <lukegtseng@gmail.com>
2025-09-11 03:27:35 -0700

    removing debug lines from index.ts

--- frontend/lib/api/index.ts
+++ frontend/lib/api/index.ts
@@ -1,8 +1,5 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
-
 export class ApiError extends Error {
     constructor(
         message: string,
