5936541f4c63a23839f38a4a44f00fb2492bb5ec
Chanseok Oh <chanseokpoh@gmail.com>
2025-09-30 11:44:10 -0700

    linter fixed but any for meta

--- frontend/components/tic-tac-toe/board.tsx
+++ frontend/components/tic-tac-toe/board.tsx
@@ -5,22 +5,22 @@ import { Cell, CellValue } from "@/components/tic-tac-toe/cell";
 export type SmallBoard = (CellValue | null)[][];
 
 type Props = {
-  board: SmallBoard;
-  onCellClick: (index: number) => void;
-  isBoardClickable: boolean;
+    board: SmallBoard;
+    onCellClick: (index: number) => void;
+    isBoardClickable: boolean;
 };
 
 export function Board({ board, onCellClick, isBoardClickable }: Props) {
-  return (
-    <div className="grid grid-cols-3">
-      {board.flat().map((cell, i) => (
-        <Cell
-          key={i}
-          value={cell}
-          onClick={() => onCellClick(i)}
-          isClickable={isBoardClickable && !cell}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className="grid grid-cols-3">
+            {board.flat().map((cell, i) => (
+                <Cell
+                    key={i}
+                    value={cell}
+                    onClick={() => onCellClick(i)}
+                    isClickable={isBoardClickable && !cell}
+                />
+            ))}
+        </div>
+    );
 }
--- frontend/components/tic-tac-toe/cell.tsx
+++ frontend/components/tic-tac-toe/cell.tsx
@@ -6,23 +6,23 @@ import { cn } from "@/lib/utils";
 export type CellValue = "X" | "O" | "-" | null;
 
 type Props = {
-  value: CellValue;
-  onClick: () => void;
-  isClickable: boolean;
+    value: CellValue;
+    onClick: () => void;
+    isClickable: boolean;
 };
 
 export function Cell({ value, onClick, isClickable }: Props) {
-  return (
-    <Button
-      variant="outline"
-      className={cn(
-        "h-24 w-24 rounded-none border border-slate-300 text-6xl",
-        value === "X" ? "text-blue-500" : "text-red-500"
-      )}
-      onClick={onClick}
-      disabled={!isClickable}
-    >
-      {value}
-    </Button>
-  );
+    return (
+        <Button
+            variant="outline"
+            className={cn(
+                "h-24 w-24 rounded-none border border-slate-300 text-6xl",
+                value === "X" ? "text-blue-500" : "text-red-500"
+            )}
+            onClick={onClick}
+            disabled={!isClickable}
+        >
+            {value}
+        </Button>
+    );
 }
