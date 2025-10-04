0a47b5bf5c9ce1a7e067f19ce0a410baa218c09f
Chanseok Oh <chanseokpoh@gmail.com>
2025-10-03 22:44:31 -0700

    chat-view -> chat \n\nAccomodates the custom event in websocket-provider. \nzod for parsing chat message schema out. \nUtilizes callback from websoocket-provider

---
+++ frontend/components/chat/chat.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import { useWebSocket } from "@/components/websocket-provider";
+import {
+    Chat as ChatType,
+    SendChatMessagePayload,
+} from "@/lib/types";
+
+import { MessageSchema, Message as MessageType } from "@/lib/schemas"
+import { MessageInput } from "./message-input";
+import { MessageList } from "./message-list";
+
+interface ChatProps {
+    chat: ChatType;
+    currentUserId: string;
+}
+
+export function Chat({ chat, currentUserId }: ChatProps) {
+    const { sendMessage, connectionStatus } = useWebSocket();
+    const [messages, setMessages] = useState<MessageType[]>(chat.chat_log || []);
+    useEffect(() => {
+        const handleNewMessage = (event: Event) => {
+            if (event instanceof CustomEvent) {
+                const newMessage = event.detail;
+
+                // Validate message
+                const result = MessageSchema.safeParse(newMessage);
+
+                // Update log only when it has a valid structure (in the future with multiple chat, need to check which chat to update)
+                if (result.success) {
+                    setMessages(prevMessages => [...prevMessages, newMessage]);
+                }
+            }
+
+        };
+
+        window.addEventListener("websocket-message", handleNewMessage);
+
+        // Clean up the listener when the component unmounts
+        return () => {
+            window.removeEventListener("websocket-message", handleNewMessage);
+        };
+    }, []);
+
+    const handleSendMessage = (messageText: string) => {
+        const messagePayload: SendChatMessagePayload = {
+            chat_id: chat.id,
+            sender: currentUserId,
+            message: messageText,
+        };
+        if (connectionStatus === "connected") {
+            sendMessage({ type: "chat_message", payload: messagePayload });
+        } else {
+            console.error("Connection unstable. Failed to send the message.")
+        }
+    };
+
+    return (
+        <div className="flex flex-col h-full">
+            <MessageList messages={messages} currentUserId={currentUserId} />
+            <MessageInput onSendMessage={handleSendMessage} isLoading={false} />
+        </div>
+    );
+}
